c8bc450fb658754e09c1530dfb3a6a14b51a50e5
Alexey <a.malitskiy.test@gmail.com>
2021-03-09 06:04:47 +0200

    removed old code

--- test-parsing/app/Http/Controllers/HomeController.php
+++ test-parsing/app/Http/Controllers/HomeController.php
@@ -2,45 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use App\Repositories\Posts;
-use Illuminate\Http\Request;
-use App\Services\Parsing;
-use App\Repositories\Interfaces\PostsInterface;
-use App\Jobs\GetParseContent;
 
 class HomeController extends Controller
 {
-    private $postsInterface;
-
-//    public function __construct(PostsInterface $postsInterface)
-//    {
-//        $this->postsInterface = $postsInterface;
-//    }
+    /**
+     * @param Posts $posts
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+     */
     public function index(Posts $posts)
     {
         $allPosts = $posts->getPosts();
 
-//        dd(gettype($allPosts));
         return view('index')->with('allPosts', $allPosts);
-//        $test = new Parsing();
-//        dd($test->makeParsing());
-////        dd(gettype($postsInterface->setPosts()));
-////        GetParseContent::dispatch();
-////        $testContent = new Parsing();
-////        dd($testContent);
-////        dd($testContent->makeParsing());
-//        $rolation = Post::where('id', '2')->first();
-//        $tag = $rolation->tags()->get();
-////        dd($tag['tag']);
-//        foreach ($tag as $t) {
-////            dd($t->tag);
-//        }
-////        dd(gettype($rolation));
-
-//        return view('welcome', compact('testContent'));
     }
 
+    /**
+     * @param $postNumber
+     * @param Posts $posts
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+     */
     public function show($postNumber, Posts $posts)
     {
         if(!empty($postNumber)) {
